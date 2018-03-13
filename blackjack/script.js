c2b39ee29ed2385827b69018de0ea0c37adfaad9
Congwen Ma <congwen.ma@hotmail.com>
2018-03-12 23:27:26 -0500

    broke the game down futher

--- blackjack/script.js
+++ blackjack/script.js
@@ -3,12 +3,32 @@ import Card, { ORTHODOX_DECK, getSuitIcon, getSuitColor } from "../lib/card.js";
 // debugger;
 window.Card = Card;
 window.ORTHODOX_DECK = ORTHODOX_DECK;
+const checkVictory = (playerCards, cpuCards) => {
+  const playerScore = sum(playerCards.map(c => c.points));
+  const cpuScore = sum(cpuCards.map(c => c.points));
+
+  let isVictorious;
+  if (playerScore <= 21) {
+    if (cpuScore <= 21) {
+      isVictorious = playerScore > cpuScore;
+    } else {
+      isVictorious = true;
+    }
+  } else {
+    if (cpuScore <= 21) {
+      isVictorious = false;
+    } else {
+      isVictorious = cpuScore > playerScore;
+    }
+  }
+  return [isVictorious, cpuScore, playerScore];
+};
 
 const sum = ns => ns.reduce((accu, n) => accu + n, 0);
 const app = new Vue({
   data: {
+    isDone: false,
     message: {
-      // text: "Hello Vue.js!",
       result: ""
     },
     deck: ORTHODOX_DECK,
@@ -25,31 +45,22 @@ const app = new Vue({
       this.player.cards.push(this.deck.pullOneOut());
     },
     done() {
-      const playerScore = sum(this.player.cards.map(c => c.points));
-      const cpuScore = sum(this.computer.cards.map(c => c.points));
-
-      let isVictorious;
-      if (playerScore <= 21) {
-        if (cpuScore <= 21) {
-          isVictorious = playerScore > cpuScore;
-        } else {
-          isVictorious = true;
-        }
-      } else {
-        if (cpuScore <= 21) {
-          isVictorious = false;
-        } else {
-          isVictorious = cpuScore > playerScore;
-        }
-      }
-      this.message.result = isVictorious ? "You Won!!!" : "You LOSE :)";
-      this.message.result += `| score is ${playerScore} to ${cpuScore}`;
-      this.player.cards = [];
-      this.computer.cards = [];
-      this.deck.reset();
+      this.isDone = true;
+      const [isVictorious, cpuScore, playerScore] = checkVictory(
+        this.player.cards,
+        this.computer.cards
+      );
+      this.message.result =
+        (isVictorious ? "You Won!!!" : "You LOSE :)") +
+        `| score is ${playerScore} to ${cpuScore}`;
 
       setTimeout(() => {
-        // get around the system and call lifecycle
+        this.isDone = false;
+        this.player.cards = [];
+        this.computer.cards = [];
+        this.deck.reset();
+
+        // got around the system and call lifecycle
         this.$options.created[0].call(this);
         this.message.result = "";
       }, 2000);
@@ -79,5 +90,54 @@ Vue.component("card-item", {
   }
 });
 
+Vue.component("card-deck", {
+  props: ["deck"],
+  template: `
+    <ul class="deck list-reset" @click="hitplayer()">
+      <card-item v-for="card in deck.cards" v-bind:card="card" v-bind:key="card.suit + card.rank" v-bind:facedown="true">
+      </card-item>
+    </ul>
+  `,
+  methods: {
+    hitplayer() {
+      this.$emit("hitplayer");
+    }
+  }
+});
+
+Vue.component("cpu-hand", {
+  props: ["hand", "isdone"],
+  template: `
+    <div class="inline-block" style="width:49%">
+      <h2>Computer</h2>
+      <ul class="hand list-reset">
+        <card-item
+          v-for="(card, index) in hand"
+          v-bind:card="card"
+          v-bind:key="card.suit + card.rank"
+          v-bind:facedown="index === 0 && !isdone"
+        >
+        </card-item>
+      </ul>
+    </div>
+  `
+});
+
+Vue.component("player-hand", {
+  props: ["hand"],
+  template: `
+    <div class="inline-block" style="width:49%">
+      <h2>Human</h2>
+      <ul class="hand list-reset">
+        <card-item
+          v-for="card in hand"
+          v-bind:card="card"
+          v-bind:key="card.suit + card.rank"
+        >
+        </card-item>
+      </ul>
+    </div>
+  `
+});
+
 app.$mount("#app");
-setTimeout(() => (app.message.text = "hello something different"), 2000);
